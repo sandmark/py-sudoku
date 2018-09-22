f3c9ed8b35304042c0168d1d42233561c8058fc6
sandmark <sandmark.m@gmail.com>
2018-09-22 20:12:51 +0900

    Add `solve` method to solve Sudoku problem

--- sudoku.py
+++ sudoku.py
@@ -1,3 +1,10 @@
+import math
+
+
+class SolvedException(Exception):
+    pass
+
+
 class Sudoku:
     """数独の問題を持ち、解決を行う。
 
@@ -13,6 +20,58 @@ class Sudoku:
         for y in range(9):
             for x in range(9):
                 self._table[x][y] = serialized[x][y]
+
+    def solve(self):
+        def solver(n, table):
+            if n == 9 * 9:
+                raise SolvedException(table)
+            else:
+                x = math.floor(n / 9)
+                y = n % 9
+                if table[x][y] != 0:
+                    return solver(n + 1, table)
+                else:
+                    for i in range(1, 10):
+                        if self.__is_valid(i, (x, y)):
+                            table[x][y] = i
+                            solver(n + 1, table)
+                            table[x][y] = 0
+
+        try:
+            solver(0, self.table[:])
+        except SolvedException as e:
+            self._table = e.args[0]
+
+    def __is_valid(self, n, pos):
+        """table[x,y]にnが矛盾なく配置できる場合は真を返す"""
+        row = self.__is_valid_row(n, pos[0])
+        col = self.__is_valid_col(n, pos[1])
+        box = self.__is_valid_box(n, pos)
+        return row and col and box
+
+    def __is_valid_row(self, n, x):
+        """行にnが配置できる場合は真を返す"""
+        for i in range(9):
+            if self.table[x][i] == n:
+                return False
+        return True
+
+    def __is_valid_col(self, n, y):
+        """列にnが配置できる場合は真を返す"""
+        for i in range(9):
+            if self.table[i][y] == n:
+                return False
+        return True
+
+    def __is_valid_box(self, n, pos):
+        """枠内にnが配置できる場合は真を返す"""
+        x = (math.floor(pos[0] / 3) * 3)
+        y = (math.floor(pos[1] / 3) * 3)
+        for i in range(2):
+            for j in range(2):
+                if self.table[x + i][y + j] == n:
+                    return False
+        return True
 
     @property
     def table(self):
--- tests/test_sudoku.py
+++ tests/test_sudoku.py
@@ -5,8 +5,34 @@ from sudoku import Sudoku
 class TestSudoku:
     """Sudokuオブジェクトのメソッドテスト"""
 
+    PROBLEM = """001078000
+005204000
+000000906
+050860407
+026001030
+104309020
+512980743
+768035090
+430127060"""
+
+    SOLVED = [[3, 9, 1, 6, 7, 8, 2, 5, 4],
+              [6, 8, 5, 2, 9, 4, 3, 7, 1],
+              [2, 4, 7, 5, 1, 3, 9, 8, 6],
+              [9, 5, 3, 8, 6, 2, 4, 1, 7],
+              [8, 2, 6, 7, 4, 1, 5, 3, 9],
+              [1, 7, 4, 3, 5, 9, 6, 2, 8],
+              [5, 1, 2, 9, 8, 6, 7, 4, 3],
+              [7, 6, 8, 4, 3, 5, 1, 9, 2],
+              [4, 3, 9, 1, 2, 7, 8, 6, 5]]
+
     def setup(self):
         self.sudoku = Sudoku()
+
+    def test_solve(self):
+        """数独問題を解決する"""
+        self.sudoku.set_numbers(self.PROBLEM)
+        self.sudoku.solve()
+        eq_(self.SOLVED, self.sudoku.table)
 
     def test_table_length(self):
         """sudoku.tableは9x9の二次元配列を持つ"""
