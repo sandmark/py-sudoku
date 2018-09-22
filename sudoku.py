b3a4d200573d62de2fa96cff1c70a5d9d803af85
sandmark <sandmark.m@gmail.com>
2018-09-22 13:45:26 +0900

    Add `set_numbers` method to initialize sudoku problem

--- sudoku.py
+++ sudoku.py
@@ -5,7 +5,14 @@ class Sudoku:
     """
 
     def __init__(self):
-        self._table = [[0 for i in range(9)] for j in range(9)]
+        self._table = [[0 for _ in range(9)] for _ in range(9)]
+
+    def set_numbers(self, string):
+        """tableに数値をセットする"""
+        serialized = [[int(i) for i in l] for l in string.split('\n')]
+        for y in range(9):
+            for x in range(9):
+                self._table[x][y] = serialized[x][y]
 
     @property
     def table(self):
--- tests/test_sudoku.py
+++ tests/test_sudoku.py
@@ -1,4 +1,4 @@
-from nose.tools import eq_
+from nose.tools import *
 from sudoku import Sudoku
 
 
@@ -13,3 +13,20 @@ class TestSudoku:
         eq_(9, len(self.sudoku.table))
         for line in self.sudoku.table:
             eq_(9, len(line))
+
+    def test_set_numbers_with_valid_string(self):
+        """sudoku.tableに9x9の数値をセットする"""
+        string = '\n'.join(['1' * 9 for _ in range(9)])
+        numbers = [[1 for _ in range(9)] for _ in range(9)]
+        self.sudoku.set_numbers(string)
+        eq_(numbers, self.sudoku.table)
+
+    @raises(IndexError)
+    def test_set_numbers_with_invalid_length(self):
+        """不正な長さの入力は例外が発生する"""
+        self.sudoku.set_numbers('129387')
+
+    @raises(ValueError)
+    def test_set_numbers_with_invalid_sequence(self):
+        """数値以外の文字を含めると例外が発生する"""
+        self.sudoku.set_numbers('asd')
